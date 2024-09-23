43772aa6f4950c0217d3c1f70d7c069fff86caa9
sourav <souravchongrey@gmail.com>
2024-09-23 16:49:06 +0530

    start my dev and mongodb server

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,13 +1,17 @@
- import mongoose from "mongoose";
- const conncetDB = async ()=> {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+import mongoose from "mongoose";
 
-     } catch(err) {
-        console.log("mongoDB connection error: ",err.message) // console.log(err)
-        process.exit(1)   // Exit process with failure
-    }
+const connectDB = async () => {
+  try {
+    console.log("mongo_uri:", process.env.MONGO_URI);
+    
+    // Await the connection promise to ensure it resolves before accessing properties
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
 
-}
-export default conncetDB
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
-
-import  conncetDB  from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
+import   connectDB   from "./db/connectDB.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const PORT = process.env.PORT || 5000;
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 // allow us to parse incoming requests with JSON payloads
-
+app.get("/", (req, res) => {
+    res.send({
+        message: "Hello World",});
+});
 
 app.use("/api/auth",authRoutes);
-conncetDB()
-app.listen(PORT, () =>
-    console.log("Server started on port ", PORT),
+app.listen(PORT, () =>{
+    connectDB ();
+    console.log("Server started on port ", PORT);
+});
 
-);
-// EcRh0fg6UjUkqhiB
+//n6MA7jHZoD6B3SQy
 // souravchongrey
